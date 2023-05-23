2f859c3fb6b0d4d117edcfb18cdbd5463c3a416b
kulwinder29 <kulwinder29august1999@gmail.com>
2023-05-23 16:42:44 +0530

    image upload fix

--- app/Http/Controllers/CustomerController.php
+++ app/Http/Controllers/CustomerController.php
@@ -8,122 +8,122 @@ use Illuminate\Http\Request;
 
 class CustomerController extends Controller
 {
-  public function index()
-  {
-    $customer = Customer::all();
-    $url = url('register');
-    $title = 'Registration';
-    $data = compact('customer', 'url', 'title');
-    return view('form')->with($data);
-  }
-
-  public function store(Request $request)
-  {
-    // Insert Query
-
-    echo $request->file('image')->store('uploads');
-    // p($request->all());
-    // die;
-    $customer = new customer;
-    $customer->u_name = $request['name'];
-    $customer->u_email = $request['email'];
-    $customer->img = url('/storage/app/uploads') ."/". $request->file('image')->getClientOriginalName();
-    $customer->gender = $request['gender'];
-    $customer->address = $request['address'];
-    $customer->country = $request['country'];
-    $customer->state = $request['state'];
-    $customer->dob = $request['dob'];
-    $customer->password = md5($request['password']);
-    $customer->save();
-    return redirect('register/view');
-  }
-
-  public function view()
-  {
-    // Select Query
-    $customer = Customer::all();
-    $data = compact('customer');
-    return view('select')->with($data);
-  }
-
-  public function trash()
-  {
-    // Select Trash Query
-    $customer = Customer::onlyTrashed()->get();
-    $data = compact('customer');
-    return view('trash_user')->with($data);
-  }
-
-  public function show($id)
-  {
-    // Select Query
-    $customer = Customer::find($id);
-    // echo '<pre>';
-    // print_r($customer);
-    // die;
-    $data = compact('customer');
-    return view('show_user')->with($data);
-  }
-
-  public function delete($id)
-  {
-    // Trash Query
-    $customer = Customer::find($id);
-    if (!is_null($customer)) {
-      $customer->delete();
+    public function index()
+    {
+        $customer = Customer::all();
+        $url = url('register');
+        $title = 'Registration';
+        $data = compact('customer', 'url', 'title');
+        return view('form')->with($data);
     }
-    return redirect('register/view');
-  }
 
-  public function forceDelete($id)
-  {
-    // forceDelete Query
-    $customer = Customer::withTrashed()->find($id);
-    if (!is_null($customer)) {
-      $customer->forceDelete();
+    public function store(Request $request)
+    {
+        // Insert Query
+
+        echo $request->file('image')->store('uploads');
+        // p($request->all());
+        // die;
+        $customer = new customer;
+        $customer->u_name = $request['name'];
+        $customer->u_email = $request['email'];
+        $customer->img = url('/storage/app/uploads') . "/" . $request->file('image')->getClientOriginalName();
+        $customer->gender = $request['gender'];
+        $customer->address = $request['address'];
+        $customer->country = $request['country'];
+        $customer->state = $request['state'];
+        $customer->dob = $request['dob'];
+        $customer->password = md5($request['password']);
+        $customer->save();
+        return redirect('register/view');
     }
-    return redirect('register/view');
-  }
 
-  public function restore($id)
-  {
-    // Restore Query
-    $customer = Customer::withTrashed()->find($id);
-    if (!is_null($customer)) {
-      $customer->restore();
+    public function view()
+    {
+        // Select Query
+        $customer = Customer::all();
+        $data = compact('customer');
+        return view('select')->with($data);
     }
-    return redirect('register/view');
-  }
 
-  public function edit($id)
-  {
-    // Edit
-    $customer = Customer::find($id);
-    if (!is_null($customer)) {
-      $url = url('customer/update') . '/' . $id;
-      $title = 'Update User';
-      $data = compact('customer', 'url', 'title');
-      return view('form')->with($data);
-    } else {
-      return redirect('register/view');
+    public function trash()
+    {
+        // Select Trash Query
+        $customer = Customer::onlyTrashed()->get();
+        $data = compact('customer');
+        return view('trash_user')->with($data);
     }
-  }
 
-  public function update($id, Request $request)
-  {
-    // Update Query
-    echo $request->file('image')->store('uploads');
-    $customer = Customer::find($id);
-    $customer->u_name = $request['name'];
-    $customer->u_email = $request['email'];
-    $customer->img = url('/storage/app/uploads') ."/". $request->file('image')->getClientOriginalName();
-    $customer->gender = $request['gender'];
-    $customer->address = $request['address'];
-    $customer->country = $request['country'];
-    $customer->state = $request['state'];
-    $customer->dob = $request['dob'];
-    $customer->password = md5($request['password']);
-    $customer->save();
-    return redirect('register/view');
-  }
+    public function show($id)
+    {
+        // Select Query
+        $customer = Customer::find($id);
+        // echo '<pre>';
+        // print_r($customer);
+        // die;
+        $data = compact('customer');
+        return view('show_user')->with($data);
+    }
+
+    public function delete($id)
+    {
+        // Trash Query
+        $customer = Customer::find($id);
+        if (!is_null($customer)) {
+            $customer->delete();
+        }
+        return redirect('register/view');
+    }
+
+    public function forceDelete($id)
+    {
+        // forceDelete Query
+        $customer = Customer::withTrashed()->find($id);
+        if (!is_null($customer)) {
+            $customer->forceDelete();
+        }
+        return redirect('register/view');
+    }
+
+    public function restore($id)
+    {
+        // Restore Query
+        $customer = Customer::withTrashed()->find($id);
+        if (!is_null($customer)) {
+            $customer->restore();
+        }
+        return redirect('register/view');
+    }
+
+    public function edit($id)
+    {
+        // Edit
+        $customer = Customer::find($id);
+        if (!is_null($customer)) {
+            $url = url('customer/update') . '/' . $id;
+            $title = 'Update User';
+            $data = compact('customer', 'url', 'title');
+            return view('form')->with($data);
+        } else {
+            return redirect('register/view');
+        }
+    }
+
+    public function update($id, Request $request)
+    {
+        // Update Query
+        echo $request->file('image')->store('uploads');
+        $customer = Customer::find($id);
+        $customer->u_name = $request['name'];
+        $customer->u_email = $request['email'];
+        $customer->img = url('/storage/app/uploads') . "/" . $request->file('image')->getClientOriginalName();
+        $customer->gender = $request['gender'];
+        $customer->address = $request['address'];
+        $customer->country = $request['country'];
+        $customer->state = $request['state'];
+        $customer->dob = $request['dob'];
+        $customer->password = md5($request['password']);
+        $customer->save();
+        return redirect('register/view');
+    }
 }
